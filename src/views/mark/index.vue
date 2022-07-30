350de32ac51da1dc19ef3099be2e47524c1ede9c
qrw <2645175553@qq.com>
2022-07-31 00:33:34 +0800

    7/31 0:33

--- src/views/mark/index.vue
+++ src/views/mark/index.vue
@@ -23,15 +23,19 @@
         </el-select>
       </el-col>
       <!-- 班级 -->
-      <el-col :span="7" :push="1">
+      <el-col :span="3" :push="1">
         <el-select v-model="selected_class" filterable allow-create default-first-option placeholder="选择或输入班别"
           :clearable="true" @change="class_change">
           <el-option v-for="item in option_class" :key="item" :label="item" :value="item">
           </el-option>
         </el-select>
       </el-col>
+      <!-- 备注 -->
+      <el-col :span="7" :push="7">
+        <el-input v-model="remarks"></el-input>
+      </el-col>
       <!-- 提交 -->
-      <el-col :span="7" :push="11">
+      <el-col :span="3" :push="7">
         <el-button type="primary">提交</el-button>
       </el-col>
     </el-row>
@@ -52,11 +56,11 @@
       <el-table-column prop="" label="打分对象" min-width="15%">
         <template slot-scope="{row, $index}">
           <el-switch v-model="personal[$index]" active-text="个人" inactive-text="集体" active-color="#13ce66"
-            inactive-color="#ff4949">
+            inactive-color="#ff4949" @change="active_change">
           </el-switch>
         </template>
       </el-table-column>
-      <el-table-column prop="" label="学生" min-width="15%">
+      <el-table-column prop="" label="学生" min-width="15%" v-for="item in column_student">
         <template slot-scope="{row, $index}">
           <el-select v-model="selected_student" filterable allow-create default-first-option placeholder="选择或输入学生"
             :clearable="true" @change="student_change" v-show="personal[$index]">
@@ -106,7 +110,9 @@ export default {
       default_value: [1, 2, 3],
       min: [1, -100, -100],
       max: [100, 2, 100],
-      personal: [false, false, false]
+      personal: [false, false, false],
+      column_student: [],
+      remarks: ''
     };
   },
   methods: {
@@ -133,8 +139,6 @@ export default {
           this.max.push(100);
         }
       }
-      console.log(this.range);
-
     },
     grade_change(value) {//更新年级
       //更新数据
@@ -310,6 +314,16 @@ export default {
           }
         }
       }
+    },
+    active_change() {//更新列的扩展
+      for (let item of this.personal) {
+        if (item === true) {//遇到一个需要选择学生就需要扩展列
+          this.column_student = [];
+          this.column_student.push(true);
+          return;
+        }
+      }
+      this.column_student = [];
     }
   }
 };
