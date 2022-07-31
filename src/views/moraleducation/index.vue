2a4cd83f50adcd07a9eda52ecc0cfa96093b220b
qrw <2645175553@qq.com>
2022-07-31 09:57:06 +0800

    7/31 9:56

--- src/views/moraleducation/index.vue
+++ src/views/moraleducation/index.vue
@@ -10,17 +10,17 @@
         <!-- span -->
         <span
           style="margin-top: 20px;height: 40px;font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;font-weight: bolder;position: absolute;left:10px;">德育量化指标</span>
-        <!-- 一级指标 -->
+        <!-- 添加一级指标 -->
         <span style="position: absolute;right:10px;margin-top: 10px;">
           <el-popover placement="bottom" width="300" v-model="root_visible" @hide="clean_holder">
             <el-input v-model="item" placeholder="新添加的指标"></el-input>
             <div style="text-align: right; margin-top: 5px;">
               <el-button size="mini" type="text" @click="root_cancel(data)"><i class="el-icon-close"></i>
               </el-button>
-              <el-button type="primary" size="mini" @click="root_item(data)"><i class="el-icon-check"></i>
+              <el-button type="primary" size="mini" @click="root_confirm(data)"><i class="el-icon-check"></i>
               </el-button>
             </div>
-            <el-button type="text" slot="reference" class="node-function">添加一级指标</el-button>
+            <el-button type="text" slot="reference" style="font-size: 16px;">添加一级指标</el-button>
           </el-popover>
         </span>
       </div>
@@ -107,8 +107,111 @@
         </span>
       </el-tree> -->
 
-      <el-tree :data="data" :props="defaultProps" default-expand-all></el-tree>
+      <el-tree :data="data" :props="defaultProps" node-key="id" default-expand-all class="filter-tree"
+        :filter-node-method="filterNode" ref="tree" :expand-on-click-node="false">
+        <div slot-scope="{ node, data }" class="custom-tree-node">
+          <el-tooltip placement="top" effect="light">
+            <div slot="content">
+              <p>指标名称:{{ node.label }}</p>
+              <p>{{ data.level }}级指标</p>
+              <p v-if="data.level === 3">检查内容:{{ data.content }}</p>
+              <p v-if="data.level === 3">default_value:{{ data.default_value }}</p>
+              <p v-if="data.level === 3">5</p>
+            </div>
+            <span>{{ node.label }}</span>
+          </el-tooltip>
+          <span>
+            <!-- 添加二级指标 -->
+            <el-popover placement="bottom" width="300" v-model="add_visible[data.id]" @hide="clean_holder"
+              v-if="node.level === 1">
+              <el-input v-model="item" placeholder="二级指标"></el-input>
+              <div style="text-align: right; margin-top: 5px;">
+                <el-button size="mini" type="text" @click="add_cancel(data)"><i class="el-icon-close"></i>
+                </el-button>
+                <el-button type="primary" size="mini" @click="add_confirm(data)"><i class="el-icon-check"></i>
+                </el-button>
+              </div>
+              <el-button type="text" slot="reference" class="node-function" icon="el-icon-circle-plus-outline">
+              </el-button>
+            </el-popover>
+
+            <!-- 添加三级指标 -->
+            <el-popover placement="bottom" width="400" v-model="third_visible[data.id]" @hide="clean_holder"
+              v-else-if="node.level === 2">
+              <el-input v-model="info.label" placeholder="三级指标"></el-input>
+              <el-input v-model="info.content" placeholder="检查内容"></el-input>
+              <el-radio-group v-model="info.allow">
+                <el-radio label="add">仅加分</el-radio>
+                <el-radio label="sub">仅减分</el-radio>
+                <el-radio label="all">都可以</el-radio>
+              </el-radio-group>
+              <el-select v-model="info.step">
+                <el-option v-for="item in 5" :key="item" :label="item" :value="item"></el-option>
+              </el-select>
+              <el-input v-model="info.default_value" placeholder="初始值"></el-input>
+              <div style="text-align: right; margin-top: 5px;">
+                <el-button size="mini" type="text" @click="third_cancel(data)"><i class="el-icon-close"></i>
+                </el-button>
+                <el-button type="primary" size="mini" @click="third_confirm(data)"><i class="el-icon-check"></i>
+                </el-button>
+              </div>
+              <el-button type="text" slot="reference" class="node-function" icon="el-icon-circle-plus-outline">
+              </el-button>
+            </el-popover>
+
+            <!-- 删除指标 -->
+            <el-popover placement="top" width="160" v-model="delete_visible[data.id]">
+              <p>确认删除？</p>
+              <div style="text-align: right; margin: 0">
+                <el-button size="mini" type="text" @click="delete_cancel(data)"><i class="el-icon-close"></i>
+                </el-button>
+                <el-button type="primary" size="mini" @click="delete_confirm(node, data)"><i class="el-icon-check"></i>
+                </el-button>
+              </div>
+              <el-button type="text" slot="reference" class="node-function" icon="el-icon-remove-outline"></el-button>
+            </el-popover>
+
+            <!-- 重命名一、二级指标 -->
+            <el-popover placement="bottom" width="300" v-model="rename_visible[data.id]" @hide="clean_holder"
+              v-if="data.level <= 2">
+              <el-input v-model="item" placeholder="修改后的指标"></el-input>
+              <div style="text-align: right; margin-top: 5px">
+                <el-button size="mini" type="text" @click="rename_cancel(data)"><i class="el-icon-close"></i>
+                </el-button>
+                <el-button type="primary" size="mini" @click="rename_cinfirm(data)"><i class="el-icon-check"></i>
+                </el-button>
+              </div>
+              <el-button type="text" slot="reference" class="node-function" icon="el-icon-edit-outline">
+              </el-button>
+            </el-popover>
+
+            <!-- 修改三级指标 -->
+            <el-popover placement="bottom" width="400" v-model="update_visible[data.id]" @hide="clean_holder" v-else>
+              <el-input v-model="info.label" placeholder="三级指标"></el-input>
+              <el-input v-model="info.content" placeholder="检查内容"></el-input>
+              <el-radio-group v-model="info.allow">
+                <el-radio label="add">仅加分</el-radio>
+                <el-radio label="sub">仅减分</el-radio>
+                <el-radio label="all">都可以</el-radio>
+              </el-radio-group>
+              <el-select v-model="info.step">
+                <el-option v-for="item in 5" :key="item" :label="item" :value="item"></el-option>
+              </el-select>
+              <el-input v-model="info.default_value" placeholder="初始值"></el-input>
+              <div style="text-align: right; margin-top: 5px;">
+                <el-button size="mini" type="text" @click="update_cancel(data)"><i class="el-icon-close"></i>
+                </el-button>
+                <el-button type="primary" size="mini" @click="update_confirm(data)"><i class="el-icon-check"></i>
+                </el-button>
+              </div>
+              <el-button type="text" slot="reference" class="node-function" icon="el-icon-circle-plus-outline">
+              </el-button>
+            </el-popover>
+          </span>
+        </div>
+      </el-tree>
     </div>
+
     <!-- 预览 -->
     <el-button type="text" @click="handle_click" style="margin: 2vh 1vw;" :loading="isloading">
       预览<i class="el-icon-notebook-2"></i>
@@ -215,14 +318,14 @@ export default {
       if (!value) return true;
       return data.label.indexOf(value) !== -1;
     },
-    root_item() {//添加一级指标
+    root_confirm() {//添加一级指标
       this.$store.commit('add_root', this.item);
       this.root_visible = false;
     },
     root_cancel() {//取消添加一级指标
       this.root_visible = false;
     },
-    add_item(data) {//添加二级指标
+    add_confirm(data) {//添加二级指标
       console.log('add_item');
       const info = {
         id: data.id,
@@ -251,7 +354,7 @@ export default {
     third_cancel(data) {
       this.$set(this.third_visible, data.id, false);
     },
-    delete_item(node, data) {//确认删除
+    delete_confirm(node, data) {//确认删除
       const info = {
         id: data.id,
         level: data.level,
@@ -266,7 +369,7 @@ export default {
     clean_holder() {
       this.item = '';
     },
-    rename_item(data) {//确认重命名
+    rename_confirm(data) {//确认重命名
       this.$set(this.rename_visible, data.id, false);
       const info = {
         id: data.id,
@@ -277,6 +380,12 @@ export default {
     },
     rename_cancel(data) {//取消重命名
       this.$set(this.rename_visible, data.id, false);
+    },
+    update_confirm(data) {
+
+    },
+    update_cancel(data) {
+
     },
     handle_click() {
       this.dialogTableVisible = true;
@@ -293,12 +402,11 @@ export default {
   align-items: center;
   justify-content: space-between;
   font-size: 16px;
-  padding-right: 8px;
-  height: 100px;
 }
 
 .node-function {
   padding-left: 10px;
+  font-size: 18px;
 }
 
 .custom-tree-container {
